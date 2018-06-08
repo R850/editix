3e75e2122e0abba4223fc0c65b444e3c89b13cec
Eric LOPEZ <eric.lopez@cgi.fr>
2018-06-08 15:55:26 +0200

    Modif pour vérifier le déclenchement automatique de Jenkins

--- createpdf.py
+++ createpdf.py
@@ -2,6 +2,7 @@ import shutil
 import os
 srcdir = "./"
 
+# debut
 for src_file in os.listdir(srcdir) :
     if src_file.endswith ('pdf') :
         shutil.copy(srcdir + src_file, srcdir + "TI" + src_file)
